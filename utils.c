b2e70149c7ede4a3aaf7bddb023bb748ce80d536
Your Name <you@example.com>
2023-05-04 00:09:54 +0700

    fix long model && fix button

--- utils.c
+++ utils.c
@@ -26,12 +26,8 @@ Vector3 centerV(Vector3 v1, Vector3 v2) {
 }
 
 void draw_model(Color color, int count_index, face indices[], float scale_p, Vector3 move, Vector3 rotate, Vector3 opt) {
-    rlPushMatrix();
-    rlCheckRenderBatchLimit(3*(count_index - 2));
-    rlBegin(RL_LINES);
-    rlColor4ub(color.r, color.g, color.b, color.a);
-    
-    for(int i = 0; i<count_index;i++) {
+    for(int i = 0; i<count_index ;i++) {
+        
         Vector3  v1 = moveV(scaleV(oper(indices[i].v1, rotate), scale_p), move);
         Vector3  v2 = moveV(scaleV(oper(indices[i].v2, rotate), scale_p), move);
         Vector3  v3 = moveV(scaleV(oper(indices[i].v3, rotate), scale_p), move);
@@ -43,18 +39,14 @@ void draw_model(Color color, int count_index, face indices[], float scale_p, Vec
         Vector3  vc23 = centerV(v2,v3);
         Vector3  vcvc232 = centerV(vc23,v2);
         Vector3  vcvc233 = centerV(vc23,v3);
-
-        rlVertex3f(v1.x,v1.y, v1.z);
-        rlVertex3f(vcvc221.x,vcvc221.y, vcvc221.z);
-        rlVertex3f(vcvc122.x,vcvc122.y, vcvc122.z);
-        rlVertex3f(v2.x,v2.y, v2.z);
-        
-        rlVertex3f(v2.x,v2.y, v2.z);
-        rlVertex3f(vcvc232.x,vcvc232.y, vcvc232.z);
-        rlVertex3f(vcvc233.x,vcvc233.y, vcvc233.z);
-        rlVertex3f(v3.x,v3.y, v3.z);
-        
+        if(opt.x){
+            DrawLine3D(v1,v2,GREEN);
+            DrawLine3D(v2,v3,GREEN);
+        } else {
+            DrawLine3D(v1,vcvc122,GREEN);
+            DrawLine3D(vcvc221,v2,GREEN);
+            DrawLine3D(v2,vcvc232,GREEN);
+            DrawLine3D(vcvc233,v3,GREEN);
         }
-    rlEnd();
-    rlPopMatrix();
+        }
 }
